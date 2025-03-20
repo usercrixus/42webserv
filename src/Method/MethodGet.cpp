ed2c104ace247f3be84a303b8fdbf3f445129919
Gaetan Morel <gmorel@paul-f3Ar3s1.clusters.42paris.fr>
2025-03-20 14:33:50 +0100

    comments

--- src/Method/MethodGet.cpp
+++ src/Method/MethodGet.cpp
@@ -19,6 +19,15 @@ void MethodGet::handle()
 	std::stringstream buffer;
 	std::string path = Data::getInstance()->getHttp().getServers()[_request.getServerId()].getRoot() + _request.getPath();
 	std::cout << path << std::endl;
+	/* Gerer le routage qui peut etre different en fonction de la location 
+	s'il y a un autre root dedans, ne pas prendre le root general "multipage-website" tout le temps, 
+	ca gerera donc egalement le moment ou il faut appeler le CGI 
+	Exemple :    location /content {
+            	root   /var/www/content;
+            	autoindex  off;
+            	index      index.html;
+        		} 
+	--> Root sur /var/www/content quand location /content correspond au path*/
 	if (path.compare(0, 4, "/cgi") == 0) //a modifier
 	{
 		Cgi Cgi(_request, path);
