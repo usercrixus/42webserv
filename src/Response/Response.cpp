78813e3193df3c74000d9d788d75c98b88271427
Chaisneau <chaisneau@gmail.com>
2025-03-24 01:01:14 +0100

    share bug on redirection

--- src/Response/Response.cpp
+++ src/Response/Response.cpp
@@ -96,7 +96,6 @@ void Response::setHeader(int statusCode, std::string path, const std::map<std::s
     setContentLen();
     setConnection();
     setCookies(cookies);
-	std::cout << _headers << std::endl;
 }
 
 
